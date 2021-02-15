76865e80a8406aca928b10e0981d3b3d638cdf78
Matheus Gabriel <matheusg663@gmail.com>
2021-02-15 12:00:36 -0300

    15/02/2021

--- src/model/entitites/Reservation.java
+++ src/model/entitites/Reservation.java
@@ -1,7 +1,7 @@
 package model.entitites;
 
 import java.text.SimpleDateFormat;
- import java.util.Date;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
@@ -46,14 +46,22 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public void updateDates(Date checkIn, Date checkOut) {
-		this.checkIn = checkIn;
-		this.checkOut = checkOut;
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now))
+			return "Error in reservation: Reservation dates for update must be future dates.";
+		if (!checkOut.after(checkIn))
+			return "Error in reservation: Check-out date must be after check-in date";
+			this.checkIn = checkIn;
+			this.checkOut = checkOut;
+			
+		return null;
+
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Room " + rommNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut)
-				+ ", "+duration() +" nights";
+				+ ", " + duration() + " nights";
 	}
 }

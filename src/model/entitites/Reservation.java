4ac35d37a1232bec754c0a93ced3cab6427045e3
Matheus Gabriel <matheusg663@gmail.com>
2021-02-15 11:42:43 -0300

    15/02/2021

---
+++ src/model/entitites/Reservation.java
@@ -0,0 +1,59 @@
+package model.entitites;
+
+import java.text.SimpleDateFormat;
+ import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+	static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	private int rommNumber;
+	private Date checkIn;
+	private Date checkOut;
+
+	public Reservation(int rommNumber, Date checkIn, Date checkOut) {
+		this.rommNumber = rommNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public int getRommNumber() {
+		return rommNumber;
+	}
+
+	public void setRommNumber(int rommNumber) {
+		this.rommNumber = rommNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public void setCheckIn(Date checkIn) {
+		this.checkIn = checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+
+	public void setCheckOut(Date checkOut) {
+		this.checkOut = checkOut;
+	}
+
+	public long duration() {
+		long diff = checkOut.getTime() - checkIn.getTime();
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room " + rommNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut)
+				+ ", "+duration() +" nights";
+	}
+}
